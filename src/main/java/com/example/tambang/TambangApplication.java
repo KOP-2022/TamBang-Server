99da65efeca35cf37b688d1b12a47c33b50c7d92
dongHun <actgong@gmail.com>
2023-01-06 23:06:03 +0900

    initial commit

---
+++ src/main/java/com/example/tambang/TambangApplication.java
@@ -0,0 +1,13 @@
+package com.example.tambang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TambangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TambangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tambang/TambangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tambang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TambangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
