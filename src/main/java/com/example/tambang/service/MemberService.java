3def20cc096fa56e110e29eaa86cee3c3e38c710
dongHun <actgong@gmail.com>
2023-01-21 19:35:30 +0900

    [update] member service test 구현

--- src/main/java/com/example/tambang/service/MemberService.java
+++ src/main/java/com/example/tambang/service/MemberService.java
@@ -14,9 +14,19 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional(readOnly = false)
-    public String join(Member member){
+    public String join(Member member) throws IllegalStateException{
+        validateDuplicateMember(member);
         //memberRepository 계층을 이용해 member entity를 DB에 저장
         memberRepository.save(member);
         return member.getId();
+    }
+
+    //중복 아이디를 검사하는 method
+    private void validateDuplicateMember(Member member){
+        Member findMember = memberRepository.findOne(member.getId());
+
+        if(findMember != null){
+            throw new IllegalStateException("이미 존재하는 아이디입니다.");
+        }
     }
 }
